dd5d4582308817bb9470d0a20ab40fb443d9488f
BizMagnets <bizmagnets@BizMagnetss-Mac-mini.local>
2025-07-14 09:15:05 +0530

    first commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by BizMagnets on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
